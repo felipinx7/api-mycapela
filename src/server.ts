386d68759f0a79e600d04f6261371fbd03ffcf59
Felipe Dev <flimalimafelipek@gmail.com>
2026-07-23 17:41:19 -0300

    feat: mudanças nos arquivos de configuração e server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { routerCapela } from "./routes/RotasCapela";
 
-const app = express();
+// variaveis de configurações.
+export const app = express();
 const port = process.env.PORT || 3000;
 
+// Inicializando configurações do projeto.
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use("/admin/", routerCapela);
+
+// Inicialização do servidor.
 app.listen(port, () => {
   console.log(`Servidor Rodando Na Porta ${port}`);
 });
