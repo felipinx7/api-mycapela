05b59dd2e479360975fa86cab5cd5c6a4bfb7831
Felipe Dev <flimalimafelipek@gmail.com>
2026-07-23 17:33:58 -0300

    feat: arquivos de configuração do projeto

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,10 +1,8 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+const adapter = new PrismaPg({ connectionString });
+export const prisma = new PrismaClient({ adapter });
