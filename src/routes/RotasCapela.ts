4d421819c2877e141a20aeaf73d51aa6ac0f7ef3
Felipe Dev <flimalimafelipek@gmail.com>
2026-07-24 11:42:35 -0300

    feat: implementação de novas rotas da aplicação

--- src/routes/RotasCapela.ts
+++ src/routes/RotasCapela.ts
@@ -1,6 +1,31 @@
 import { routes } from "../config/rotas";
 import { CriarCapelaController } from "../controllers/capela/createCapelaController";
+import { DeleteCapelaController } from "../controllers/capela/deleteCapelaController";
+import { PegarCapelasController } from "../controllers/capela/getCapelasController";
+import { PegarUnicaCapelaController } from "../controllers/capela/getUniqueCapelaController";
+import { AtualizarCapelaController } from "../controllers/capela/updateCapelaController";
 
+export const routerCapela = routes;
+routerCapela.post(
+  "/criar",
+  async (req, res) => await CriarCapelaController({ req, res }),
+);
+routerCapela.delete(
+  "/deletar",
+  async (req, res) => await DeleteCapelaController({ req, res }),
+);
 
-export const routerCapela = routes
-routerCapela.post("/criar", async (req, res) => await CriarCapelaController({req, res}))   
+routerCapela.get(
+  "/pegar",
+  async (req, res) => await PegarUnicaCapelaController({ req, res }),
+);
+
+routerCapela.get(
+  "/pegar-todas",
+  async (req, res) => await PegarCapelasController({ req, res }),
+);
+
+routerCapela.post(
+  "/atualizar",
+  async (req, res) => await AtualizarCapelaController({ req, res }),
+);
