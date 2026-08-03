52097455c389c20cbdbccc84dff53649d20e42c7
Felipe Dev <flimalimafelipek@gmail.com>
2026-08-03 19:34:52 -0300

    fix: correção do nome da função da retorno da minha API

--- src/utils/ResposeDasRequisicoes.ts
+++ src/utils/ResposeDasRequisicoes.ts
@@ -1,6 +1,6 @@
 import { InterfaceDadosRespostaDasRequisicoes } from "../interfaces/DadosRespostaDasRequisicoes";
 
-export function RespstaDasRequisicoes({ message, status, data, express }: InterfaceDadosRespostaDasRequisicoes) {
+export function RespostasDasRequisicoes({ message, status, data, express }: InterfaceDadosRespostaDasRequisicoes) {
   if (data === undefined) {
     return express?.res.status(status).send({
       status: status,
